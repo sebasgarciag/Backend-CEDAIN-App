915b0e0095dbb78474041f838f7b4a8c459e1070
Jesus Horacio <a01566851@tec.mx>
2023-09-28 19:54:43 -0600

    CED-51 arreglar cambios salida controller

--- controllers/salida.controller.js
+++ controllers/salida.controller.js
@@ -33,3 +33,19 @@ exports.getBuscarTodosEventos = async function (req, res) {
     let Eventos = await salidaService.buscarTodosEventos();
     res.json(Eventos).status(200);
 };
+
+exports.postSalidasDetalles = async function (req, res){
+    let result = validationResult(req);
+
+    console.log("======================");
+    console.log("postSalidasDetalles");
+    console.log("======================");
+    if (result.errors.length > 0) {
+        return res.status(400).json({ success: false, error: result }); //if routes.js sends error, controller catches and sends error #.
+    } else {
+        let productosSalida = req.body;
+        let detallesCreados = await salidaService.crearSalidaDetalle(productosSalida);
+        res.status(201).json(detallesCreados);
+    } 
+
+};
