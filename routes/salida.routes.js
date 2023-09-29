e7d5b49a8030d803c14cf06333fc708ba5f4c8eb
alexis <alexisgargo3@gmail.com>
2023-09-28 21:17:46 -0600

    ced-11 fixed salidas

--- routes/salida.routes.js
+++ routes/salida.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { check, param } = require('express-validator');
 let salidaController = require("../controllers/salida.controller");
 
-router.get("/salidas", salidaController.getBuscarTodas);
+router.get("", salidaController.getBuscarTodas);
 router.get("/comunidades", salidaController.getBuscarTodasComunidades);
 router.get("/eventos", salidaController.getBuscarTodosEventos);
 
