2cb4c26ee76a1d0bc956fbe279f46e5bb2e85240
Angel Corrales <73620647+AngelCorso@users.noreply.github.com>
2023-10-02 00:20:31 -0600

    Chernobyl (#17)
    * Avance Endpoint SignUp
    
    * Avances
    
    * CA-8 GET and PUT User implementation
    
    Done CA-8
    Done CA-30
    Done CA-31
    
    * Avance x2
    
    * Resolviendo conflictos
    
    * Resolver problema no entra json
    
    * Login
    
    * Quitar console.log
    
    * giulianaCambiosBack
    
    * giulianaCambiosBack
    
    ---------
    
    Co-authored-by: SalvadoRobles03 <99054999+SalvadoRobles03@users.noreply.github.com>
    Co-authored-by: Giuliana <gherreralpez@gmail.com>
    Co-authored-by: giucomhubb <106790165+giucomhubb@users.noreply.github.com>

---
+++ controllers/usuario.controller.js
@@ -0,0 +1,129 @@
+const usuarioService = require("../services/usuario.service");
+const { validationResult } = require("express-validator");
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+//Crea Usuario
+exports.postCrearUsuario = async function (req, res) {
+
+
+  try {
+    let newUsuario = req.body;     //todo lo que viene en el json payload
+
+    // Verifica si la contraseña existe antes de hashearla
+    if (newUsuario.password) {
+      newUsuario.password = bcrypt.hashSync(newUsuario.password, saltRounds);
+    } else {
+      throw new Error('Contraseña no proporcionada');
+    }
+
+    let usuarioCreado = await usuarioService.crearUsuario(newUsuario);
+    return res.json(usuarioCreado).status(201);
+  }
+  catch (error) { //En caso de error relacionado a la base de datos, enter here.
+    console.error("Error al intentar crear usuario: ", error);
+    console.log("Error al intentar crear usuario: ", error);
+    return res.status(500).json({ success: false, message: "Error durante proceso de crear usuario" });
+  }
+};
+
+
+//Login
+exports.postLogin = async function (req, res) {
+
+  try {
+    let loginData = req.body;     //todo lo que viene en el json payload
+
+    // Verifica si la contraseña existe
+    if (!loginData.password) {
+      throw new Error('Contraseña no proporcionada');
+    }
+
+    // Busca al usuario en la base de datos
+    let usuario = await usuarioService.buscarUsuarioPorCorreo(loginData.correo);
+
+    if (!usuario) {
+      return res.status(404).json({ success: false, message: "Usuario no encontrado" });
+    }
+
+    // Compara la contraseña proporcionada con la contraseña encriptada almacenada
+    let passwordCorrecta = bcrypt.compareSync(loginData.password, usuario.password);
+
+    if (!passwordCorrecta) {
+      return res.status(401).json({ success: false, message: "Contraseña incorrecta" });
+    }
+
+    // Si todo está bien, regresa true
+    return res.json({ success: true }).status(200);
+  }
+  catch (error) { //En caso de error relacionado a la base de datos, enter here.
+    console.error("Error al intentar iniciar sesión: ", error);
+    console.log("Error al intentar iniciar sesión: ", error);
+    return res.status(500).json({ success: false, message: "Error durante proceso de inicio de sesión" });
+  }
+};
+
+
+
+
+
+
+// GET ALL USERS
+exports.getBuscarTodos = async function (req, res) {
+  let usuarios = await usuarioService.buscarTodos();
+  if (usuarios == null) {
+    res.status(503).json({ error: "Base de datos no disponible" });
+  } else if (usuarios.length == 0) {
+    res.status(204).json({ Message: "No se encontraron datos" });
+  } else {
+    res.status(200).json(usuarios);
+  }
+};
+
+//GET USER BY ID
+exports.getBuscarPorId = async function (req, res) {
+  let result = validationResult(req);
+
+  if (result.errors.length > 0) {
+    res.status(400).json({ success: false, error: result });
+  } else {
+    let id_usuario = req.params.id;
+    let usuario = await usuarioService.buscarPorId(id_usuario);
+
+    if (usuario == null) {
+      res.status(404).json({"error": "usuario no encontrado"});
+    } else if (usuario == false) {
+      res.status(503).json({ "error": "Base de datos no disponible" });
+    } else {
+      res.status(200).json(usuario);
+    }
+  }
+};
+
+//UPDATE EXISTING USER
+exports.updateUsuario = async function (req, res) {
+  let result = validationResult(req);
+
+  if (result.errors.length > 0) {
+    res.status(400).json({ success: false, error: result }); //aqui manda los errores
+  } else {
+    let usuario = req.body;
+    let id_usuario = req.params.id;
+
+    const usuario_actualizado = await usuarioService.updateUsuario(
+      id_usuario,
+      usuario
+    );
+
+    if (usuario_actualizado == null) {
+      return res.status(404).json({ 'error': "Usuario no encontrado" });
+    } else if (usuario_actualizado == false) {
+      return res.status(503).json({ 'error': "Base de datos no disponible" });
+    } else {
+      return res.status(200).json({ success: true }); // Devuelve el registro actualizado
+    }
+  }
+
+
+};
---
+++ routes/usuario.routes.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const { check, param } = require('express-validator');
+
+let usuarioController = require("../controllers/usuario.controller");
+
+// GET ALL USERS
+router.get("/", usuarioController.getBuscarTodos);
+
+// //GET USER BY ID
+router.get("/:id", [ 
+    param("id").isNumeric().withMessage("ID debe ser numerico")
+], usuarioController.getBuscarPorId);
+
+//UPDATE EXISTING USER
+router.put("/:id", [
+    param("id").isNumeric().withMessage("Id debe ser numerico"),
+    check("nombre").optional().isLength({max:40}).withMessage("nombre  no debe exceder 40 caracteres"),
+    check("apellido_paterno").optional().isLength({ max: 40 }).withMessage("apellido_paterno no debe exceder 40 caracteres"),
+    check("apellido_materno").optional().isLength({ max: 40 }).withMessage("apellido_materno no debe exceder 40 caracteres"),
+    check("tipo").optional().isLength({ max: 40 }).withMessage("tipo no debe exceder 40 caracteres"),
+    check("password").optional().isLength({max:255}).withMessage("password  no debe exceder 255 caracteres"),
+    check("id_almacen").optional().isNumeric().withMessage("id_almacen debe ser numérico"),
+], usuarioController.updateUsuario);
+
+
+
+router.post("/newUsuario", [
+    check("nombre").notEmpty().isLength({ max: 40 }).withMessage("nombre  no debe exceder 40 caracteres"),
+    check("apellido_paterno").notEmpty().isLength({ max: 40 }).withMessage("apellido_paterno no debe exceder 40 caracteres"),
+    check("apellido_materno").notEmpty().isLength({ max: 40 }).withMessage("apellido_materno no debe exceder 40 caracteres"),
+    check("tipo").notEmpty().isLength({ max: 40 }).withMessage("tipo no debe exceder 40 caracteres"),
+    check("id_almacen").notEmpty().isNumeric().withMessage("id_almacen debe ser numérico"),
+    check("correo").notEmpty().withMessage("correo no debe estar vacio").isEmail().withMessage("correo debe ser un correo valido"),
+    check("password").notEmpty().isLength({ max: 255 }).withMessage("password no debe exceder 255 caracteres"),
+
+], usuarioController.postCrearUsuario);
+
+router.post("/login", [
+    
+    check("correo").notEmpty().withMessage("correo no debe estar vacio").isEmail().withMessage("correo debe ser un correo valido"),
+    check("password").notEmpty().isLength({ max: 255 }).withMessage("password no debe exceder 255 caracteres"),
+
+], usuarioController.postLogin);
+
+module.exports = router;
