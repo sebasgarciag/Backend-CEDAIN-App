bf9d13b5501ac8cb3fdbd79d9dd31f50ff92f1d4
Nacho <A00756853@tec.mx>
2023-10-12 19:20:59 -0600

    se avanzo a k6 pruebas de performance

--- K6/scripts.js
+++ K6/scripts.js
@@ -1,16 +1,34 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-const API_URL = "https://backend-cedain-app-xcfpf63vha-uc.a.run.app";
+//const API_URL = "https://backend-cedain-app-xcfpf63vha-uc.a.run.app";
+const API_URL = "http://localhost:8080";
+
 
 export const options = {
     scenarios: {
-      getEntradas: {  //Test Case 1
-        executor: 'constant-vus',
-        exec: 'getEntradas',
-        vus: 30,
-        duration: '30s',
-      }
+        //POSTS
+        postEntrada: {  //Test Case 1a
+            executor: 'constant-vus',
+            exec: 'crearEntrada',
+            vus: 10,
+            duration: '30s',
+          },
+
+
+
+
+
+
+        //GETS
+        getEntradas: {  //Test Case 1b
+            executor: 'constant-vus',
+            exec: 'getEntradas',
+            vus: 10,
+            duration: '30s',
+        },
+
+
     }
   }
 
@@ -19,5 +37,22 @@ export const options = {
     check(
       response, {"GET entradas status code is 200": (r) => r.status == 200}
     );
+    sleep(0.5);
+  }
+
+  export function crearEntrada() {
+    const primerNombre = "Test";
+    const apellido = new Date().getTime().toString();
+  
+    const data = { nombre: primerNombre, apellido: apellido };
+  
+    const response = http.post(`${API_URL}/personas/`, JSON.stringify(data), {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    check(
+      response, {"POST personas status code is 201": (r) => r.status == 201}
+    );
+  
     sleep(0.5);
   }
