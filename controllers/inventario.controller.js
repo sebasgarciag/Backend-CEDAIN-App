a8f42a9de0de5ab5a6b350ae38e9935133c169a8
Jesus Horacio <a01566851@tec.mx>
2023-10-13 18:29:50 -0600

    CED-89 Arreglado modificar inventario controller

--- controllers/inventario.controller.js
+++ controllers/inventario.controller.js
@@ -56,22 +56,41 @@ exports.postCrear = async function (req, res) {
     }    
 };
 
-exports.putModificarInventario = async function (req,res){
+exports.putModificarInventario = async function (req, res){
     let result = validationResult(req);
 
     if (result.errors.length > 0) {
-        res.status(400).json({ success: false, error: result }); //aqui manda los errores
+        return res.status(400).json({ success: false, error: result });
     } else {
-        let inventario = req.body;
-        let idInventario = req.params.id;
-        let inventarioActualizado = await inventarioService.updateInventario(idInventario,inventario);
+        const inventario = req.body; // Puede ser un objeto o un arreglo de objetos.
 
-        if (inventarioActualizado == true) {
+        if (Array.isArray(inventario)) {
+            // Si inventario es un arreglo de objetos, procesa cada producto por separado.
+            for (const producto of inventario) {
+                const idProducto = producto.id_producto;
+                const idAlmacen = producto.id_almacen;
+
+                const inventarioModificado = await inventarioService.actualizarExistencias(producto, idProducto, idAlmacen);
+
+                if (inventarioModificado == false) {
+                    return res.status(204).json({ success: false });
+                }
+            }
+
+            // Si todos los productos se actualizaron correctamente, envía una respuesta exitosa.
             res.status(200).json({ success: true });
         } else {
-            res.status(204).json({ success: false });
-        }        
+            // Si inventario es un solo objeto, procesa ese producto.
+            const idProducto = inventario.id_producto;
+            const idAlmacen = inventario.id_almacen;
 
-    }    
-    
+            const inventarioModificado = await inventarioService.actualizarExistencias(inventario, idProducto, idAlmacen);
+
+            if (inventarioModificado == true) {
+                res.status(200).json({ success: true });
+            } else {
+                res.status(204).json({ success: false });
+            }
+        }
+    }
 };
